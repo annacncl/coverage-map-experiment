987bf70b4d631009867e30a589979c8d53e44765
annacncl <annac@ncl.org>
2026-03-02 07:47:27 -0500

    Create orgCoverage.js

---
+++ orgCoverage.js
@@ -0,0 +1,5 @@
+window.orgCoverage = {
+  org_001: { countyIds: ["25017", "25025", "9001"], stateIds: [] },
+  org_002: { countyIds: ["25017"], stateIds: [] },
+  org_003: { countyIds: [], stateIds: ["25", "23", "8", "51"] }
+};
