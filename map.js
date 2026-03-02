66d6fd4917ad608953411ec719e0d98cd7dd5ceb
annacncl <annac@ncl.org>
2026-03-02 07:45:23 -0500

    Update map.js

--- map.js
+++ map.js
@@ -28,4 +28,19 @@ map.on("load", () => {
     }
   });
 
+  // 👇 ADD THIS PART
+  map.on("click", "org-pins", (e) => {
+
+    const feature = e.features[0];
+
+    const coordinates = feature.geometry.coordinates.slice();
+    const name = feature.properties.name;
+
+    new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML(`<strong>${name}</strong>`)
+      .addTo(map);
+
+  });
+
 });
